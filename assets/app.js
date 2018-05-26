eba915feaf66c67b324362e87ef39a46fcc200d4
William Bork <williambork33@gmail.com>
2018-05-26 16:07:20 -0500

    Added Firebase to JS file

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,13 @@
-asdf
+
+//Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDIkqoeImNZjQYXjEzH9-jAqVh3EvUFrK8",
+    authDomain: "timesheet-851e3.firebaseapp.com",
+    databaseURL: "https://timesheet-851e3.firebaseio.com",
+    projectId: "timesheet-851e3",
+    storageBucket: "timesheet-851e3.appspot.com",
+    messagingSenderId: "988140441751"
+  };
+
+
+  firebase.initializeApp(config);
